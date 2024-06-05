5de351a59972d9bf330261536eb5a3f41ef7a961
Alexandro <alexandro_cristian@hotmail.com>
2024-06-04 22:54:47 -0300

    atualizacao

--- setPath.bat
+++
@@ -1,3 +0,0 @@
-@cd C:\Users\Alexandro.Carraro\Downloads\Prenota bot\WPy64-31180\n\confirmation-project-main
-@set PATH=C:\Users\Alexandro.Carraro\Downloads\Prenota bot\WPy64-31180\n;%PATH%
-@cmd.exe /K
